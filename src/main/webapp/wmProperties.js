4bf3105b7a9ab27baadf1800f8c8cbe4a201aa80
root <root@04a688be5d00>
2017-03-02 04:22:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Prod 2ndMar",
+  "homePage" : "Main",
+  "name" : "Prod_2ndMar",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
