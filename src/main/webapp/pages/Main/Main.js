f071a58f1098d91573cd2480ddd571257f60a50b
Surya <surya.talluri+4@wavemaker.com>
2017-03-06 09:30:59 +0000

    1st version New files added - [dbscenarios,list,Cities,QRCode,liveform1,checkbox1,QRCode,search,checkbox,AdventureWorks2014,issue,list111,datetime,sqljdbc4-4.0.jar,intercept-urls.json,database-options.json] Files modified - [pages-config.json,Main.html,liveform.variables.json,apply.variables.json,project-services.xml,Common.variables.json,deployment.properties,securityService.properties,hrdb.properties,config.xml,form.variables.json,Login.variables.json,app.variables.json,Main.variables.json,roles.json,Alert.variables.json,leftnav.variables.json,Main.js,test.variables.json,general-options.json,config.json,Partial.variables.json,pom.xml,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,17 +1,34 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
     };
 
+
+    $scope.serviceVariable1onBeforeUpdate = function(variable, inputData) {
+        //return false;
+    };
+
+
+    $scope.serviceVariable1onError = function(variable, data) {
+        debugger;
+    };
+
 }]);
+
+Application.$controller("liveform1Controller", ["$scope",
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
+]);
